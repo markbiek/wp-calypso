3ec8df6dcc1a50a03204abdc075e12bb66d2a984
Christopher Smith <chris@chriswsmith.info>
2022-01-16 15:07:42 -0700

    UI updates

--- client/me/purchases/payment-methods/components/payment-method-edit-form-fields.tsx
+++ client/me/purchases/payment-methods/components/payment-method-edit-form-fields.tsx
@@ -1,7 +1,31 @@
+import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
 import FormTextInput from 'calypso/components/forms/form-text-input';
+import FormFieldAnnotation from 'calypso/my-sites/checkout/composite-checkout/components/form-field-annotation';
+import {
+	LeftColumn,
+	RightColumn,
+} from 'calypso/my-sites/checkout/composite-checkout/components/ie-fallback';
 import useCountryList from 'calypso/my-sites/checkout/composite-checkout/hooks/use-country-list';
 import TaxCountrySelectMenu from './tax-country-select-menu';
+
+const GridRow = styled.div`
+	display: -ms-grid;
+	display: grid;
+	width: 100%;
+	-ms-grid-columns: 48% 4% 48%;
+	grid-template-columns: 48% 48%;
+	grid-column-gap: 4%;
+	justify-items: stretch;
+`;
+
+const FieldRow = styled( GridRow )`
+	margin-top: 16px;
+
+	:first-of-type {
+		margin-top: 0;
+	}
+`;
 
 const RenderEditFormFields = ( {
 	onChangePostalCode,
@@ -15,8 +39,8 @@ const RenderEditFormFields = ( {
 	postalCodeValue: string;
 	countryCodeValue: string;
 } ): JSX.Element => {
-	const translate = useTranslate();
 	const countriesList = useCountryList( [] );
+	const translate = useTranslate();
 
 	if ( ! postalCodeValue ) {
 		postalCodeValue = '';
@@ -24,23 +48,37 @@ const RenderEditFormFields = ( {
 
 	return (
 		<>
-			<TaxCountrySelectMenu
-				name="tax_country_code"
-				translate={ translate }
-				onChange={ onChangeCountryCode }
-				isError={ '' }
-				isDisabled={ false }
-				errorMessage={ '' }
-				currentValue={ countryCodeValue || '' }
-				countriesList={ countriesList }
-			/>
-
-			<FormTextInput
-				name="tax_postal_code"
-				placeholder="Enter postal code"
-				value={ '' || postalCodeValue }
-				onChange={ onChangePostalCode }
-			/>
+			<FieldRow>
+				<LeftColumn>
+					<TaxCountrySelectMenu
+						name="tax_country_code"
+						translate={ translate }
+						onChange={ onChangeCountryCode }
+						isError={ '' }
+						isDisabled={ false }
+						errorMessage={ '' }
+						currentValue={ countryCodeValue || '' }
+						countriesList={ countriesList }
+					/>
+				</LeftColumn>
+				<RightColumn>
+					<FormFieldAnnotation
+						labelText={ translate( 'Postal Code', { textOnly: true } ) }
+						formFieldId="tax_postal_code"
+						labelId="tax_postal_code_label"
+						descriptionId="tax_postal_code_label"
+						errorDescription=""
+					>
+						<FormTextInput
+							id="tax_postal_code"
+							name="tax_postal_code"
+							placeholder="Enter postal code"
+							value={ '' || postalCodeValue }
+							onChange={ onChangePostalCode }
+						/>
+					</FormFieldAnnotation>
+				</RightColumn>
+			</FieldRow>
 		</>
 	);
 };
