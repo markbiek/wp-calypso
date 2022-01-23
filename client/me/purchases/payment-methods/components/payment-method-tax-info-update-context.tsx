0eae4cbdca91b1fb2097c4f72b9137ca661b7b5c
Christopher Smith <chris@chriswsmith.info>
2022-01-22 21:55:25 -0700

    Remove unneeded context provider.

--- client/me/purchases/payment-methods/components/payment-method-tax-info-update-context.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const TaxInfoUpdateContextTemplate = false;
-
-const TaxInfoUpdateContext = createContext< boolean >( TaxInfoUpdateContextTemplate );
-
-export default TaxInfoUpdateContext;
