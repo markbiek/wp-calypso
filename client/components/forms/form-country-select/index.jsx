3a28abbae04c645d1335cd20252c79001153167b
Christopher Smith <chris@chriswsmith.info>
2022-01-23 01:18:04 -0700

    Refactor to functional component

--- client/components/forms/form-country-select/index.jsx
+++ client/components/forms/form-country-select/index.jsx
@@ -1,26 +1,14 @@
 import classnames from 'classnames';
-// import { localize } from 'i18n-calypso';
 import { useTranslate } from 'i18n-calypso';
 import { isEmpty, omit } from 'lodash';
-import PropTypes from 'prop-types';
-import { Component } from 'react';
 import FormSelect from 'calypso/components/forms/form-select';
 
 import './style.scss';
 
-export class FormCountrySelect extends Component {
-	static translate = useTranslate();
-	static propTypes = {
-		countriesList: PropTypes.array.isRequired,
-		className: PropTypes.string,
-		disabled: PropTypes.bool,
-		onChange: PropTypes.func,
-		translate: PropTypes.func.isRequired,
-	};
+function FormCountrySelect( { countriesList, className, onChange, disabled } ) {
+	const translate = useTranslate();
 
-	getOptions() {
-		const { countriesList, translate } = this.props;
-
+	function getOptions() {
 		if ( isEmpty( countriesList ) ) {
 			return [
 				{
@@ -38,32 +26,30 @@ export class FormCountrySelect extends Component {
 		} ) );
 	}
 
-	render() {
-		const options = this.getOptions();
+	const options = getOptions();
 
-		return (
-			<FormSelect
-				{ ...omit( this.props, [
-					'className',
-					'countriesList',
-					'translate',
-					'moment',
-					'numberFormat',
-				] ) }
-				className={ classnames( this.props.className, 'form-country-select' ) }
-				onChange={ this.props.onChange }
-				disabled={ this.props.disabled }
-			>
-				{ options.map( function ( option ) {
-					return (
-						<option key={ option.key } value={ option.code } disabled={ option.disabled }>
-							{ option.label }
-						</option>
-					);
-				} ) }
-			</FormSelect>
-		);
-	}
+	return (
+		<FormSelect
+			{ ...omit( { countriesList, className, onChange, disabled }, [
+				'className',
+				'countriesList',
+				'translate',
+				'moment',
+				'numberFormat',
+			] ) }
+			className={ classnames( className, 'form-country-select' ) }
+			onChange={ onChange }
+			disabled={ disabled }
+		>
+			{ options.map( function ( option ) {
+				return (
+					<option key={ option.key } value={ option.code } disabled={ option.disabled }>
+						{ option.label }
+					</option>
+				);
+			} ) }
+		</FormSelect>
+	);
 }
 
 export default FormCountrySelect;
