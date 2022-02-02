2d45360cdbc429d7079364c640412e6e43847cda
Dale du Preez <dale@automattic.com>
2022-02-02 12:04:17 +0200

    Broaden types accepted by hasGSuiteWithUs()

--- client/lib/gsuite/has-gsuite-with-us.js
+++ client/lib/gsuite/has-gsuite-with-us.js
@@ -3,7 +3,7 @@ import { getGSuiteSubscriptionStatus } from 'calypso/lib/gsuite/get-gsuite-subsc
 /**
  * Given a domain object, does that domain have G Suite with us.
  *
- * @param {import('../domains/types').ResponseDomain | undefined } domain - Domain object
+ * @param {import('../domains/types').ResponseDomain | import('calypso/state/sites/domains/types').SiteDomain | undefined } domain - Domain object
  * @returns {boolean} - true if the domain is under our management, false otherwise
  */
 export function hasGSuiteWithUs( domain ) {
