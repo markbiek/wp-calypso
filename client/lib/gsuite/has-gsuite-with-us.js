91d67ffcf09b5e9106c3eff2b6d42b12523c940c
Dale du Preez <dale@automattic.com>
2022-02-02 10:41:13 +0200

    Update type details for hasGSuiteWithUs() and hasGSuiteWithAnotherProvider()

--- client/lib/gsuite/has-gsuite-with-us.js
+++ client/lib/gsuite/has-gsuite-with-us.js
@@ -3,7 +3,7 @@ import { getGSuiteSubscriptionStatus } from 'calypso/lib/gsuite/get-gsuite-subsc
 /**
  * Given a domain object, does that domain have G Suite with us.
  *
- * @param {undefined|{googleAppsSubscription?:{status?: string}}} domain - Domain object
+ * @param {import('../domains/types').ResponseDomain | undefined } domain - Domain object
  * @returns {boolean} - true if the domain is under our management, false otherwise
  */
 export function hasGSuiteWithUs( domain ) {
